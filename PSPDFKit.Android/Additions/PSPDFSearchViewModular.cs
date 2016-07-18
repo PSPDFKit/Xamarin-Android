61d20e5717f6c9347772578c17f772d26e9fcf10
Alex Soto <alex@alexsoto.me>
2016-07-18 00:47:22 -0500

    [PSPDFKit.Android] Fix AbstractSearchView and friends
    ref: http://stackoverflow.com/q/5260467/572076

---
+++ PSPDFKit.Android/Additions/PSPDFSearchViewModular.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.Runtime;
+
+namespace PSPDFKit.UI.Search {
+	public sealed partial class PSPDFSearchViewModular {
+		static IntPtr id_isShown;
+		public override unsafe bool IsShown {
+			[Register ("isShown", "()Z", "GetIsShownHandler")]
+			get {
+				if (id_isShown == IntPtr.Zero)
+					id_isShown = JNIEnv.GetMethodID (class_ref, "isShown", "()Z");
+				try {
+					return JNIEnv.CallBooleanMethod (Handle, id_isShown);
+				}
+				finally {
+				}
+			}
+		}
+	}
+}
